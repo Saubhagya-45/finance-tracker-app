62ff29cef4a6a25ca939c11566b12c07ff1592c8
Abhishk Kumar <abhishkdk@gmail.com>
2026-06-21 01:52:35 +0530

    Added DB_PORT support

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ console.log("DB:", process.env.DB_NAME);
 const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
